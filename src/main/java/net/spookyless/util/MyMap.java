bd9ca882c6aed5860a39e68301161a43ec5c16ae
Spookyless <spookyless.info@gmail.com>
2023-11-08 00:42:52 +0100

    Implemented MyMap

--- src/main/java/net/spookyless/util/MyMap.java
+++ src/main/java/net/spookyless/util/MyMap.java
@@ -1,31 +1,61 @@
 package net.spookyless.util;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class MyMap<K, V> implements Map<K, V> {
+    private final ArrayList<K> keys = new ArrayList<>();
+    private final ArrayList<V> values = new ArrayList<>();
 
     @Override
     public boolean put(K key, V value) {
-        return false;
+        int index = keys.indexOf(key);
+
+        if(key == null || value == null) {
+            return false;
+        }
+
+        if(index == -1) {
+            keys.add(key);
+            values.add(value);
+        } else {
+            values.add(index, value);
+        }
+
+        return true;
     }
 
     @Override
     public boolean remove(K key) {
-        return false;
+        int index = keys.indexOf(key);
+
+        if(index == -1) {
+            return false;
+        }
+
+        keys.remove(index);
+        values.remove(index);
+        return true;
     }
 
     @Override
     public V get(K key) {
-        return null;
+        int index = keys.indexOf(key);
+
+        if(index > -1) {
+            return values.get(index);
+        } else {
+            return null;
+        }
     }
 
     @Override
     public List<K> keys() {
-        return null;
+        return (List<K>) keys.clone();
     }
 
     @Override
     public boolean contains(K key) {
-        return false;
+        return keys.contains(key);
     }
 }
--- src/test/java/net/spookyless/util/MyMapTest.java
+++ src/test/java/net/spookyless/util/MyMapTest.java
@@ -2,7 +2,6 @@ package net.spookyless.util;
 
 import org.junit.jupiter.api.*;
 
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
